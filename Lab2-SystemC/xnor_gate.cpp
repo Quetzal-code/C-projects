989356ef991d980dd657d246c29d9ab5a5a6a560
Quetzal <quetzcode@gmail.com>
2025-11-06 21:04:40 -0400

    X
    Update

--- Lab2-SystemC/xnor_gate.cpp
+++ Lab2-SystemC/xnor_gate.cpp
@@ -1,11 +1,11 @@
 #include <systemc.h>
-SC_MODULE(OrGate) {
+SC_MODULE(XnorGate) {
 sc_in<bool> a, b;
 sc_out<bool> y;
 void do_or() {
 y.write(a.read() || b.read());
 }
-SC_CTOR(OrGate) {
+SC_CTOR(XnorGate) {
 SC_METHOD(do_or);
 sensitive << a << b;
 }
--- Lab2-SystemC/xor_gate.cpp
+++ Lab2-SystemC/xor_gate.cpp
@@ -1,11 +1,11 @@
 #include <systemc.h>
-SC_MODULE(OrGate) {
+SC_MODULE(XorGate) {
 sc_in<bool> a, b;
 sc_out<bool> y;
 void do_or() {
 y.write(a.read() || b.read());
 }
-SC_CTOR(OrGate) {
+SC_CTOR(XorGate) {
 SC_METHOD(do_or);
 sensitive << a << b;
 }
